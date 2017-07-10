a3422cce113a66c53d98043892a24d0a04ea783e
Matt Reishman <matt.reishman@Me.com>
2017-07-10 09:14:11 -0500

    #2

---
+++ local/default/conf/topConfig.php
@@ -0,0 +1,9 @@
+
+	<?php
+		$topConfig = array(
+			'pollingRateOverviewMain' => '1000',
+			'pollingRateOverviewSlow' => '10000',
+			'pollingRateOverviewMainType'	=> 'Milliseconds',
+			'pollingRateOverviewSlowType'	=> 'Milliseconds'
+			);
+	?>
