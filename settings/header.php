d2adeb7d794ea4f9f02dcc2d9f24a3374fc18f80
Matt Reishman <matt.reishman@Me.com>
2017-08-02 14:37:27 -0500

    fixed issue with settings header

--- settings/header.php
+++ settings/header.php
@@ -5,10 +5,10 @@ $URI = $_SERVER['REQUEST_URI'];
 	<div onclick="goToUrl('../index.php');" style="display: inline-block; cursor: pointer; height: 26px; width: 26px; margin-left: 5px;">
 		<img id="pauseImage" class="menuImage" src="../core/img/backArrow.png" height="26px" style=" margin-top: -2px; ">
 	</div>
-	<?php if(strpos($URI, 'main.php') !== false): ?>
+	<?php if(strpos($URI, 'settingsTop.php') !== false): ?>
 		<a style="cursor: default;" class="active" id="mainLink" >Main</a>
 	<?php else: ?>
-		<a id="mainLink" onclick="goToUrl('main.php');" >Main</a>
+		<a id="mainLink" onclick="goToUrl('settingsTop.php');" >Main</a>
 	<?php endif; ?>
 	<?php if(strpos($URI, 'about.php') !== false): ?>
 		<a style="cursor: default;" class="active" id="aboutLink" >About</a>
