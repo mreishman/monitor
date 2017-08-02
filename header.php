c25c81684de0c7aa5c89f9501cc154dab1e72144
Matt Reishman <matt.reishman@Me.com>
2017-08-02 15:36:44 -0500

    fixed issue with header
    - moved all other pages behind overview (except settings)
    - added seperator between pages and links to other apps

--- header.php
+++ header.php
@@ -49,7 +49,22 @@ $URI = $_SERVER['REQUEST_URI'];
 </div>
 <?php else: ?>
 <div id="menu">
-	<?php $notThisPage = true; ?>
+	<?php $notThisPage = true;
+	if((strpos($URI, 'settings.php') !== false) || (strpos($URI, 'php.php') !== false) || (strpos($URI, 'network.php') !== false) || (strpos($URI, 'ram.php') !== false) || (strpos($URI, 'disk.php') !== false) || (strpos($URI, 'cpu.php') !== false))
+	{
+		$notThisPage = false;
+	}
+	?>
+	<?php if(strpos($URI, 'settings.php') !== false): ?>
+		<a id="settingsLink" class="active" id="mainLink">Settings</a>
+	<?php else: ?>
+		<a id="settingsLink" onclick="window.location.href = 'settings/settingsTop.php';" >Settings</a>
+	<?php endif; ?>
+	<?php if($notThisPage): ?>
+		<a style="cursor: default;" class="active" id="mainLink" >Overview</a>
+	<?php else: ?>
+		<a id="mainLink" onclick="window.location.href =  'overview.php';" >Overview</a>
+	<?php endif; ?>
 	<!-- 
 	<?php if(strpos($URI, 'cpu.php') !== false): ?>
 		<a style="cursor: default;" class="active" id="mainLink" >CPU</a>
@@ -77,16 +92,7 @@ $URI = $_SERVER['REQUEST_URI'];
 		<a id="mainLink" onclick="window.location.href =  'php.php';" >PHP</a>
 	<?php endif; ?>
 	-->
-	<?php if(strpos($URI, 'settings.php') !== false): $notThisPage = false; ?>
-		<a id="settingsLink" class="active" id="mainLink">Settings</a>
-	<?php else: ?>
-		<a id="settingsLink" onclick="window.location.href = 'settings/settingsTop.php';" >Settings</a>
-	<?php endif; ?>
-	<?php if($notThisPage): ?>
-		<a style="cursor: default;" class="active" id="mainLink" >Overview</a>
-	<?php else: ?>
-		<a id="mainLink" onclick="window.location.href =  'overview.php';" >Overview</a>
-	<?php endif; ?>
+		<a>|</a>
 	<?php if(file_exists('../status/index.php')): ?>
 		<a id="mainLink"  onclick="window.location.href =  '../status/';" >gitStatus</a>
 	<?php endif;?>
