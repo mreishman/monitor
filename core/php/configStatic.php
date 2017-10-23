a3ab07dc2604b1f59e924dda3020d05ecad061e9
Matt Reishman <matt.reishman@Me.com>
2017-10-23 16:14:30 -0500

    update config / changelog

--- core/php/configStatic.php
+++ core/php/configStatic.php
@@ -2,10 +2,10 @@
 <?php
 
 $configStatic = array(
-  'version'   => '1.1',
-  'lastCheck'   => '10-16-2017',
-  'newestVersion' => '1.0.1',
+  'version'   => '1.1.1',
+  'lastCheck'   => '10-23-2017',
+  'newestVersion' => '1.1',
   'versionList' => array(
-  '1.0.1' => array('branchName' => '1.0.1Update','releaseNotes' => '')
+  '1.1' => array('branchName' => '1.1Update','releaseNotes' => '<ul><li>Features<ul><li>Added CPU page</li><li>Added settings option to set default sort for processes</li><li>Added delay to some of the differential status checks</li></ul></li><li>Bug Fixes<ul><li>Fixed issue with resizing the window while on index page</li></ul></li></ul>')
   )
 );
