7117e4d050f8ee0191ca44cb6df2f3f212da02fc
Matt Reishman <mreishman@users.noreply.github.com>
2017-10-23 20:30:22 -0500

    Update versionsCheckFile.php

--- versionsCheckFile.php
+++ versionsCheckFile.php
@@ -1,11 +1,15 @@
 <?php
 
 $versionCheckArray = array(
-	'version'		=> '1.1',
+	'version'		=> '1.1.1',
 	'versionList'		=> array(
 		'1.1'	        => array(
 			'branchName'	=> '1.1Update',
 			'releaseNotes'	=> '<ul><li>Features<ul><li>Added CPU page</li><li>Added settings option to set default sort for processes</li><li>Added delay to some of the differential status checks</li></ul></li><li>Bug Fixes<ul><li>Fixed issue with resizing the window while on index page</li></ul></li></ul>'
+		),
+		'1.1.1'	        => array(
+			'branchName'	=> '1.1.1Update',
+			'releaseNotes'	=> '<ul><li>Bug Fixes<ul><li>Fixed bug with sorting by some options</li><li>Fixed issue with auto check update</li><li>Fixed issue with favicon</li></ul></li></ul>'
 		),
 	)
 );
