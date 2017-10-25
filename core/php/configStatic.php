df2eaba5a7769781111da09a1e7e8368aca11cf5
Matt Reishman <matt.reishman@Me.com>
2017-10-25 14:55:40 -0500

    Small change to config file

--- core/php/configStatic.php
+++ core/php/configStatic.php
@@ -3,9 +3,9 @@
 
 $configStatic = array(
   'version'   => '1.1.1',
-  'lastCheck'   => '10-23-2017',
-  'newestVersion' => '1.1',
+  'lastCheck'   => '10-25-2017',
+  'newestVersion' => '1.1.1',
   'versionList' => array(
-  '1.1' => array('branchName' => '1.1Update','releaseNotes' => '<ul><li>Features<ul><li>Added CPU page</li><li>Added settings option to set default sort for processes</li><li>Added delay to some of the differential status checks</li></ul></li><li>Bug Fixes<ul><li>Fixed issue with resizing the window while on index page</li></ul></li></ul>')
+  '1.1' => array('branchName' => '1.1Update','releaseNotes' => '<ul><li>Features<ul><li>Added CPU page</li><li>Added settings option to set default sort for processes</li><li>Added delay to some of the differential status checks</li></ul></li><li>Bug Fixes<ul><li>Fixed issue with resizing the window while on index page</li></ul></li></ul>'),'1.1.1' => array('branchName' => '1.1.1Update','releaseNotes' => '<ul><li>Bug Fixes<ul><li>Fixed bug with sorting by some options</li><li>Fixed issue with auto check update</li><li>Fixed issue with favicon</li></ul></li></ul>')
   )
 );
