6641571802e1b0a7fd0cc709440f3bf385ad0708
Matt Reishman <mreishman@users.noreply.github.com>
2018-02-26 09:34:35 -0600

    Update configStatic.php

--- core/php/configStatic.php
+++ core/php/configStatic.php
@@ -4,7 +4,7 @@
 $configStatic = array(
   'version'   => '1.1.3',
   'lastCheck'   => '12-01-2017',
-  'newestVersion' => '1.1.2',
+  'newestVersion' => '1.1.3',
   'versionList' => array(
   '1.1' => array('branchName' => '1.1Update','releaseNotes' => '<ul><li>Features<ul><li>Added CPU page</li><li>Added settings option to set default sort for processes</li><li>Added delay to some of the differential status checks</li></ul></li><li>Bug Fixes<ul><li>Fixed issue with resizing the window while on index page</li></ul></li></ul>'),'1.1.1' => array('branchName' => '1.1.1Update','releaseNotes' => '<ul><li>Bug Fixes<ul><li>Fixed bug with sorting by some options</li><li>Fixed issue with auto check update</li><li>Fixed issue with favicon</li></ul></li></ul>'),'1.1.2' => array('branchName' => '1.1.2Update','releaseNotes' => '<ul><li>Bug Fixes<ul><li>Fixed home page issues with updater</li></ul></li></ul>')
   )
