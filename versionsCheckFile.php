88b4474b087ad8e8cae510581e55c9dd040df076
Matt Reishman <mreishman@users.noreply.github.com>
2018-09-02 09:05:00 -0500

    Update versionsCheckFile.php

--- versionsCheckFile.php
+++ versionsCheckFile.php
@@ -1,7 +1,7 @@
 <?php
 
 $versionCheckArray = array(
-	'version'		=> '1.1.4',
+	'version'		=> '1.1.5',
 	'versionList'		=> array(
 		'1.1'	        => array(
 			'branchName'	=> '1.1Update',
@@ -22,6 +22,10 @@ $versionCheckArray = array(
 		'1.1.4'	        => array(
 			'branchName'	=> '1.1.4Update',
 			'releaseNotes'	=> '<ul><li>Bug Fixes<ul><li>Fix for ioStat message if not installed</li><li>Fix for mpstat message if not installed</li></ul></li></ul>'
+		),
+		'1.1.5'	        => array(
+			'branchName'	=> '1.1.5Update',
+			'releaseNotes'	=> '<ul><li>Bug Fixes<ul><li>Style fix for low res monitors (one column)</li><li>Switched from ioStat to just reading /proc/diskstats for better compatability</li><li>Better style for scrollbars</li><li>Changed base poll request to be 1.5s from 1s (has to be greater than 1 or will start adding delays between requests)</li></ul></li></ul>'
 		),
 	)
 );
